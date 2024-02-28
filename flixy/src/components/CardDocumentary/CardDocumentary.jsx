32e2f1d17c7e9fced32cadc99ed8fb063c486618
jimemena91 <jimerodriguez1187@gmail.com>
2024-02-28 19:04:02 -0300

    creando las Cards

---
+++ flixy/src/components/CardDocumentary/CardDocumentary.jsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import React from 'react';
+import styles from './CardDocumentary.module.css';
+
+const DocumentaryCard = ({ documentary }) => {
+  return (
+    <div className={styles.documentaryCard}>
+      <Link href="/documentary/[documentaryId]" as={`/documentary/${documentary.id}`}>
+        
+          <img src={documentary.image} alt={documentary.nameSpanish || documentary.nameOriginal} className={styles.documentaryImage} />
+          <div className={styles.documentaryDetails}>
+            <p>{documentary.category.join('| ')}</p>
+            <div className={styles.streamingLogos}>
+              {documentary.streaming.map((stream, index) => (
+                <img
+                  key={index}
+                  src={`/logos/${stream}.svg`}
+                  alt={stream}
+                  className={styles.streamingLogo}
+                />
+              ))}
+            </div>
+          </div>
+        
+      </Link>
+    </div>
+  );
+};
+
+export default DocumentaryCard;
