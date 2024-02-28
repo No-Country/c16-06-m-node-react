d2b9030b9b1965768479f2ce34d7bf3cd58b12af
jimemena91 <jimerodriguez1187@gmail.com>
2024-02-28 19:21:45 -0300

    creando navbar

---
+++ flixy/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,45 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import {UserCircle} from "@phosphor-icons/react/dist/ssr";
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <Link href="/">
+          <span>
+            <Image src="/logoFlixi/Flixi.svg" alt="Logo" width={50} height={50} />
+          </span>
+        </Link>
+      </div>
+      <ul className={styles.navList}>
+        <li className={styles.navItem}>
+          <Link href="/">
+            <span>Home</span>
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href="/lo-nuevo">
+            <span>Lo Nuevo</span>
+          </Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href="/lo-popular">
+            <span>Lo Popular</span>
+          </Link>
+        </li>
+      </ul>
+     
+      <Link href="/login" >
+      <UserCircle size={32} />  
+        <span className={styles.loginLink}>Login</span>
+      </Link>
+      
+        
+        
+    </nav>
+  );
+};
+
+export default Navbar;
