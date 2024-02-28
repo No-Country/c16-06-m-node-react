c7250add5b573086c38753bea147bbad0f7233f2
jimemena91 <jimerodriguez1187@gmail.com>
2024-02-28 20:00:08 -0300

    subiendo imagenes

---
+++ flixy/src/app/[documentaryId]/page.js
@@ -0,0 +1,8 @@
+// pages/[documentaryId].js
+import DetailPage from '../../components/DetailPage/DetailPage';
+
+const DocumentaryDetail = () => {
+  return <DetailPage />;
+};
+
+export default DocumentaryDetail;
