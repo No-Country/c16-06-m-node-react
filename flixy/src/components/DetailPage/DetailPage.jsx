94d59a27155156494aef9992656d3121f2b020d4
Sergio <Sergiogutierrezp122@gmail.com>
2024-03-05 21:45:49 -0500

    Estilos de la seccion de recomendados

--- flixy/src/components/DetailPage/DetailPage.jsx
+++ flixy/src/components/DetailPage/DetailPage.jsx
@@ -9,6 +9,7 @@ import StarRatings from 'react-star-ratings'; // Importa la biblioteca
 import data from '@/data/data';
 import styles from './detailPage.module.css';
 import Rating from '../Rating/Rating';
+import Button from '../Button/Button';
 
 const DocumentaryDetailPage = () => {
   const { id } = useParams();
@@ -60,21 +61,46 @@ const [showReviews, setShowReviews] = useState(false);
         {/* Nuevo componente para mostrar el trailer */}
         <Trailer trailerUrl={selectedDocumentary.trailer} />
       </div>
-      {/* Recomendaciones con scroll horizontal */}
-      <h3>Recomendados para ti</h3>
-      <p>Te sugerimos esta selección según tus visualizaciones</p>
-      <div className={styles.recommendedList}>
-        {recommendedDocumentaries.map((doc) => (
-          <div key={doc.id} className={styles.recommendedItem}>
-            <img
-              src={doc.image2}
-              alt={`${doc.nameSpanish || doc.nameOriginal} Poster`}
-              className={styles.recommendedImage}
-            />
-            <button onClick={() => handleShowReviews(doc.id)}>Ver Reseñas</button>
-          </div>
-        ))}
-      </div>
+
+      <section>
+        {/* Recomendaciones con scroll horizontal */}
+        <h3 className={styles.sectionTitle}>Recomendados para ti</h3>
+        <p className={styles.sectionDesc}>Te sugerimos esta selección según tus visualizaciones</p>
+        <div className={styles.recommendedList}>
+          {recommendedDocumentaries.map((doc) => (
+            <div key={doc.id} className={styles.recommendedItem}>
+              <img
+                src={doc.image2}
+                alt={`${doc.nameSpanish || doc.nameOriginal} Poster`}
+                className={styles.recommendedImage}
+              />
+              <div className={styles.cardDesc}>
+                <h4 className={styles.cardTitleE}>{doc.nameOriginal}</h4>
+
+                <div className={styles.row} style={{marginTop:"20px"}}>
+                  <h5 className={styles.cardTitleS} style={{maxWidth:"280px"}}>{doc.nameSpanish}</h5> 
+                  <span className={styles.cardTitleS}>{doc.year}</span>
+                </div>
+
+                <div className={styles.row} style={{marginTop:"15px"}}>
+                  <p className={styles.cardInfo} style={{maxWidth:"280px"}}>{doc.duration}</p> 
+                  <span className={styles.cardInfo}>{doc.certification === "Not Rated" ? "Sin calificaión" : doc.certification}</span>
+                </div>
+
+                <div className={styles.row} style={{marginTop:"10px"}}>
+                  <Rating score={doc.score}/>
+                  <span style={{color:"var(--primary-500)", fontSize:"var(--body-2)"}}>{doc.score}</span>
+                </div>
+
+                <p style={{ color:"var(--neutral-200)",marginTop:"20px"}}>{doc.description}</p>
+                
+                {/* <button style={{margin:"auto 0 0 0"}} onClick={() => handleShowReviews(doc.id)}>Ver Reseñas</button> */}
+                <Button className="btn2" style={{margin:"auto 0 0 0"}}>Ver</Button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
 
       <section style={{paddingBottom: "20px"}}>
         {/* Mostrar reseñas del documental seleccionado */}
